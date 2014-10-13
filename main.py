360041f7473e58813ee4c22b5ae14f396bd15cdf
Matt Soucy <msoucy@csh.rit.edu>
2014-10-13 01:56:19 -0400

    Reorganizing some of the core loop

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 from __future__ import print_function
-import alsaaudio
+from alsaaudio import Mixer
 import time
 import os
-import serial
+from serial import Serial
 import random
 import json
-import urllib2
+from urllib2 import urlopen
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = Serial('/dev/ttyACM0', 9600)
 ser.flushInput()
 
-m = alsaaudio.Mixer(control='PCM')
+m = Mixer(control='PCM')
 
 os.remove("/tmp/mplayer.fifo")
 os.mkfifo("/tmp/mplayer.fifo")
 os.system("mplayer -idle -slave -input file=/tmp/mplayer.fifo 1&>/dev/null &")
 mplfifo = open("/tmp/mplayer.fifo", "w", 0)
 
-playTime = 0
-
 playing = False
-varID = ""
 
 # This is a list of sample songs that will randomly play if the
 # user is misidentified or does not exist!
@@ -46,10 +43,11 @@ DING_SONG = "/home/pi/ding.mp3"
 
 def get_user_song(username):
     if username:
-        homedir = os.path.expanduser("~"+username)
+        homedir = os.path.expanduser("~" + username)
         if os.path.isdir(os.path.join(homedir, "harold")):
-            playlist = [f for f in os.listdir(dafile + "/harold")
-                        if os.path.isfile(dafile + "/harold/" + f)
+            hdir = os.path.join(homedir, "harold")
+            playlist = [f for f in os.listdir(hdir)
+                        if os.path.isfile(os.path.join(hdir, f))
                         and f.endswith(SONG_EXTS)]
             return os.path.join(homedir, "harold", random.choice(playlist))
         elif os.path.isfile(os.path.join(homedir, "harold.mp3")):
@@ -57,40 +55,42 @@ def get_user_song(username):
     return random.choice(DEFAULT_SONGS)
 
 while True:
+
+    # Lower the volume during quiet hours... Don't piss off the RA!
+    m.setvolume(85 if (time.localtime().tm_hour + 1) % 24 <= 8 else 100)
+
     if not playing:
         varID = ser.readline()
         print(varID)
-        if "ready" in varID:
-            varID = ""
         # Mplayer will play any files sent to the FIFO file.
         print("loadfile", DING_SONG, file=mplfifo)
-    if (time.localtime().tm_hour + 1) % 24 <= 8:
-        # Lower the volume during quiet hours... Don't piss off the RA!
-        m.setvolume(85)
-    else:
-        m.setvolume(100)
-    if not playing and varID != "":
-        try:
-            # Use Nick Depinet's LDAP service!
-            usernameData = json.load(urllib2.urlopen('http://www.csh.rit.edu:56124/?ibutton=' + varID))
-        except urllib2.HTTPError as error:
-            # Need to check its an 404, 503, 500, 403 etc.
-            print(error.read())
-            username = ""
-        else:
-            username = usernameData['username'][0]
-            
-        # Print the user's name (Super handy for debugging...)
-        print("New User: '" + username + "'")
+        if "ready" not in varID:
+            try:
+                # Use Nick Depinet's LDAP service!
+                data = urlopen('http://www.csh.rit.edu:56124/?ibutton=' + varID)
+                usernameData = json.load(data)
+            except urllib2.HTTPError as error:
+                # Need to check its an 404, 503, 500, 403 etc.
+                print(error.read())
+                username = ""
+            except ValueError as error:
+                # Got malformed JSON somehow
+                username = ""
+            else:
+                username = usernameData['username'][0]
 
-        song = get_user_song(username)
-        print("Now playing '" + username + "'...\n")
-        print("loadfile '" + song.replace("'", "\\'") + "'", file=mplfifo)
+            # Print the user's name (Super handy for debugging...)
+            print("New User: '" + username + "'")
 
-        time.sleep(3)
-        start = time.time()
-        playing = True
-    elif playing and time.time() - start >= 25:
+            song = get_user_song(username)
+            print("Now playing '" + username + "'...\n")
+            print("loadfile '" + song.replace("'", "\\'") + "'", file=mplfifo)
+
+            time.sleep(3)
+            start = time.time()
+            playing = True
+
+    elif time.time() - start >= 25:
         # Fade out the music at the end.
         vol = int(m.getvolume()[0])
         while vol > 60:
